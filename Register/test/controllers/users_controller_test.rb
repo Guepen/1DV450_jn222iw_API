3951530a6ece17743ee34536ce2c84fadd59e17b
Jennifer Nord <jennifer.nord@live.se>
2015-01-28 21:42:54 +0100

    First controller+view
    Routing test

---
+++ Register/test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "GET /users should list all users" do
+    assert_routing '/users', {controller: "users", action: "index"}
+    get :index
+    assert_response :success
+  end
+end
