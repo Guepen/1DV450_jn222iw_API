6bf20a89c78490cb789dbb195065dfa71e48784d
Jennifer Nord <jennifer.nord@live.se>
2015-02-03 00:59:59 +0100

    ApiKey generator

--- Register/app/models/api_key.rb
+++ Register/app/models/api_key.rb
@@ -1,4 +1,12 @@
 class ApiKey < ActiveRecord::Base
-  validates :apikey, presence: true
+  before_create :generate_new_apikey
   belongs_to :user
+  validates :apikey, presence: true, uniqueness: true
+
+  def generate_new_apikey
+    #http://railscasts.com/episodes/352-securing-an-api?view=asciicast
+    begin
+      self.apikey = SecureRandom.hex
+    end while self.class.exists?(apikey: apikey)
+  end
 end
