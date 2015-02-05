890d022d62aee1859ead7c4b38d01eb05e2fab91
Jennifer Nord <jennifer.nord@live.se>
2015-02-05 15:32:37 +0100

    Admin model created

--- Register/db/schema.rb
+++ Register/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203131511) do
+ActiveRecord::Schema.define(version: 20150205142838) do
+
+  create_table "admins", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "api_keys", force: true do |t|
     t.string   "value"
